67c8e1a3eaf8f70a3521b83b5e0da3d21a0ccd89
Dev.Nah <mcthundercat99@gmail.com>
2022-03-20 04:09:35 -0400

    Added the ability to actually run the Test

--- assets/script.js
+++ assets/script.js
@@ -1,172 +1,165 @@
 // Global Variables
-const quizStart = document.getElementById("quiz-start");
-const timer = document.getElementById("quiz-time");
-const highScore = document.getElementById("quiz-score");
-const quizText = document.getElementById("quiz-body");
-const quizAnswer = document.getElementById("quiz-answerTrue");
-const quizWrong = document.getElementById("quiz-answer");
-const quizBox = document.getElementById("quiz-question-set");
-const startPage = document.getElementById("quiz-text");
-const startButton = document.getElementById("quiz-button");
-const startBox = document.getElementById("quiz-top")
-const totalTime = 75;
-const totalPoints = 0;
+var splashPage = document.getElementById("start");
+var quizBox = document.getElementById("content");
+var quizDone = document.getElementById("quiz-end");
+var startBtn = document.getElementById("quiz-start");
+var scoreBtn = document.getElementById("quiz-score");
+var questionEl = document.getElementById("quiz-text");
+var answerOneEl = document.getElementById("quiz-answerTrue");
+var answerTwoEl = document.getElementById("quiz-answer1");
+var answerThreeEl = document.getElementById("quiz-answer2");
+var answerFourEl = document.getElementById("quiz-answer3");
+var correctChoice;
+var currentAnswer;
+var totalTime = 75;
+var totalPoints = 0;
 var activeSection;
-
-// Unhide startCode
-window.onload=function(){
-    startBox.classList.remove("hidden");
-    activeSection = startBox;
-    timer.textContent = totalTime;
-
-    // if(totalPoints === null) {
-    //     totalPoints[];
-    // }
-}
-
-// Start Quiz Code
-document.getElementById("quiz-start").onclick = startQuiz;
-
-// Question Response Code
-
-
-// Code for High Score
-var score=[];
-highScore.addEventListener("click", function() {
-    console.log('Score');
-});
-
-// script for button response
-quizStart.addEventListener("click", function() {     
-    console.log('Start');
-});
-
-quizAnswer.addEventListener("click", function() {
-    console.log('Right');
-});
-
-quizWrong.addEventListener("click", function() {
-    console.log('Wrong');  
-});
+var currentQuestion = 0;
+var quizLength =3;
 
 // Questions
 let codeQuestions = [
     {
-        set: 1,
         question: "What does HTML stand for?",
         answer: "Hyper Text Markup Language",
-        true: "True",
-        wrong1: "Hyper Thyroid Makeup Laser",
-        statement1: "false",
-        wrong2: "Hyper Text Multi Language",
-        statement2: "false",
-        wrong3: "Hip Totalitarian Model Language",
-        statement3: "false",
+        wrong: [
+                "Hyper Text Markup Language",
+                "Hyper Thyroid Makeup Laser",
+                "Hyper Text Multi Language",
+                "Hip Totalitarian Model Language"]
     },
     {
-        set: 2,
         question: "What does CSS stand for?",
         answer: "Cascading Style Sheet",
-        true: "True",
-        wrong1: "Crazy Sick style",
-        statement1: "false",
-        wrong2: "Cascading Symmetry Styles",
-        statement2: "false",
-        wrong3: "Cool Skin Sheet",
-        statement3: "false",
+        wrong: [
+                "Cascading Style Sheet",
+                "Crazy Sick style",
+                "Cascading Symmetry Styles",
+                "Cool Skin Sheet"]
+
     },
     {
-        set: 3,
-        question: "Javascript first apperaed when?",
-        answer: "1995",
-        true: "True",
-        wrong1: "2000",
-        statement1: "false",
-        wrong2: "1993",
-        statement2: "false",  
-        wrong3: "2010",
-        statement3: "false",  
+        question: "Terminal is our...",
+        answer: "Friend",
+        wrong:  [
+                "Friend",
+                "Food",
+                "Precious",
+                "Frenemie"] 
     },
-    // {
-    //     set: 4,
-    //     question: "Firefox was released in?",
-    //     answer: "2002",
-    //     choices: [
-    //         "2002",
-    //         "1998",
-    //         "2006",
-    //         "1990",
-    //         ]
-    // },
-    // {
-    //     set: 5,
-    //     question: "Linux was created by?",
-    //     answer: "Linus Torvalds",
-    //     choices: [
-    //         "Linus Sebastian",
-    //         "Linus Arnesson",
-    //         "Linus Torvalds",
-    //         "Linus Roache",
-    //         ]
-    // },
-    // {
-    //     set: 6,
-    //     question: "The code <p> denotes what?",
-    //     answer: "Paragraph",
-    //     choices: [
-    //         "Picture",
-    //         "Paragraph",
-    //         "Potential",
-    //         "Private",
-    //         ]
-    // },
-    // {
-    //     set: 7,
-    //     question: "Color changes what in css?",
-    //     answer: "Text Color",
-    //     choices: [
-    //         "Border Color",
-    //         "Background Color",
-    //         "Footer Color",
-    //         "Text Color",
-    //         ]
-    // },
-    // {
-    //     set: 8,
-    //     question: "What is GIT used for?",
-    //     answer: "Traking changes to files",
-    //     choices: [
-    //         "Uploading files",
-    //         "Traking changes to files",
-    //         "Sharing notes",
-    //         "Debugging your software",
-    //         ]
-    // },
-    // {
-    //     set: 9,
-    //     question: "To Deploy a page on Github you use what section in setting?",
-    //     answer: "Pages",
-    //     choices: [
-    //         "Enviroments",
-    //         "Deploy keys",
-    //         "Webhooks",
-    //         "Pages",
-    //         ]
-    // },
-    // {
-    //     set: 10,
-    //     question: "Terminal is our...",
-    //     answer: "Friend",
-    //     choices: [
-    //         "Friend",
-    //         "Enemy",
-    //         "Father",
-    //         "Neighbor",
-    //         ]
-    // }
 ]
 
-// Save the Score
-var saveScore = function() {
-    localStorage.setItem("score", JSON.stringify(score));
+// Unhide startCode
+window.onload = function () {
+    splashPage.classList.remove("hidden");
+    activeSection = splashPage;
+    // Displays the current Time
+    time.textContent = totalTime + "sec :Remaining ";
 }
+
+// Hide and Show Function
+function reveal (thisSection) {
+    // Hide Current section
+    activeSection.classList.add("hidden");
+    // Show Next Section
+    thisSection.classList.remove("hidden");
+    activeSection = thisSection;
+}
+
+// Quiz Start
+function startQuiz () {
+    intervalID = setInterval(totalTime, 1000);
+    reveal(quizBox);
+    function startTimer(duration, display) {
+        var timer = duration, seconds;
+        setInterval(function () {
+            seconds = parseInt(timer % 60, 10);
+    
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent =  seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+            var totalTime = 60 * 5,
+            display = document.querySelector('time');
+            startTimer(totalTime, display);
+        }, 1000);
+    }
+    quizBegin();
+}
+
+function quizBegin () {
+    createQuiz(currentQuestion);
+}
+
+// Question Content
+function createQuiz (firstRight) {
+    // generate text content of question
+    var createQuestion = codeQuestions[firstRight];
+        
+        currentAnswer = createQuestion.answer;
+
+        // Test 01
+        // questionEl.innerHTML = createQuestion.question;
+        // answerOneEl.innerHTML = createQuestion.wrong[0];
+        // answerTwoEl.innerHTML = createQuestion.wrong[1];
+        // answerThreeEl.innerHTML = createQuestion.wrong[2];
+        // answerFourEl.innerHTML = createQuestion.wrong[3];
+
+        // Test 02
+        // document.getElementById("questionEl").innerHTML = codeQuestions.question;
+        // document.getElementById("answerOneEl").innerHTML = codeQuestions.wrong[0];
+        // document.getElementById("answerTwoEl").innerHTML = codeQuestions.wrong[1];
+        // document.getElementById("answerThreeEl").innerHTML = codeQuestions.wrong[2];
+        // document.getElementById("answerFourEl").innerHTML = codeQuestions.wrong[3];
+
+        // Test 03
+        // document.getElementById("questionEl").innerText;document.getElementById("questionEl").innerHTML = text;
+        // document.getElementById("answerOneEl").innerText;document.getElementById("answerOneEl").innerHTML = text;
+        // document.getElementById("answerTwoEl").innerText;document.getElementById("answerTwoEl").innerHTML = text;
+        // document.getElementById("answerThreeEl").innerText;document.getElementById("answerThreeEl").innerHTML = text;
+        // document.getElementById("answerFourEl").innerText;
+          
+
+  }
+
+function correctAnswer (trueClick) {
+    if (trueClick == currentAnswer) {
+        return true;
+    } else {return false}
+}
+
+function quizBtnRes () {
+    var isItCorrect = this.textContent;
+    currentQuestion++;
+    completeQuestion();
+}
+
+function completeQuestion () {
+    if(currentQuestion < quizLength) {
+        quizBegin();
+    }
+    else { 
+       completedQuiz();
+    }
+}
+
+// End Quiz Code
+function completedQuiz () {
+    reveal(quizDone);
+    clearInterval(intervalID);
+}
+
+function scoreBtnPro () {
+    window.confirm("Great Job! Here's Your High Score! " +  totalTime);
+}
+// Button Code
+answerOneEl.addEventListener("click", quizBtnRes);
+answerTwoEl.addEventListener("click", quizBtnRes);
+answerThreeEl.addEventListener("click", quizBtnRes);
+answerFourEl.addEventListener("click", quizBtnRes);
+
+scoreBtn.addEventListener("click", scoreBtnPro);
+startBtn.addEventListener("click", startQuiz);
